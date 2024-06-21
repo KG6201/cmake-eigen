994d6a76be05bc146492d87eb9589daba0d0ec19
KANEKO Kojiro <k.kaneko@cmt.phys.kyushu-u.ac.jp>
2024-06-21 16:43:51 +0900

    refactor: block to compare the results for serialization and parallelization
    (cherry picked from commit aed77aaf00ce6f6d2341d435cfebfa3104c1c772)

--- parallelize-eigen/main.cpp
+++ parallelize-eigen/main.cpp
@@ -9,24 +9,31 @@ int main() {
         std::cout << "Matrix size: " << size << "x" << size << std::endl;
         Eigen::MatrixXd matrix1 = Eigen::MatrixXd::Random(size, size);
         Eigen::MatrixXd matrix2 = Eigen::MatrixXd::Random(size, size);
-        Eigen::MatrixXd result(size, size);
+        Eigen::MatrixXd result_serial(size, size);
+        Eigen::MatrixXd result_parallel(size, size);
 
         double start_time, end_time;
 
         // Eigenの内部並列化を使用
-        Eigen::setNbThreads(1); // シングルスレッドで計算
-        std::cout << "Threads num: " << Eigen::nbThreads() << std::endl;
-        start_time = omp_get_wtime();
-        result = matrix1 * matrix2;
-        end_time = omp_get_wtime();
-        std::cout << "Without parallelization (Eigen single thread): " << end_time - start_time << " seconds" << std::endl;
+        {
+            Eigen::setNbThreads(1); // シングルスレッドで計算
+            std::cout << "Threads num: " << Eigen::nbThreads() << std::endl;
+            start_time = omp_get_wtime();
+            result_serial = matrix1 * matrix2;
+            end_time = omp_get_wtime();
+            std::cout << "Without parallelization (Eigen single thread): " << end_time - start_time << " seconds" << std::endl;
+        }
 
-        Eigen::setNbThreads(omp_get_max_threads()); // OpenMPの最大スレッド数を設定
-        std::cout << "Threads num: " << Eigen::nbThreads() << std::endl;
-        start_time = omp_get_wtime();
-        result = matrix1 * matrix2;
-        end_time = omp_get_wtime();
-        std::cout << "With parallelization (Eigen multi-thread): " << end_time - start_time << " seconds" << std::endl;
+        {
+            Eigen::setNbThreads(omp_get_max_threads()); // OpenMPの最大スレッド数を設定
+            std::cout << "Threads num: " << Eigen::nbThreads() << std::endl;
+            start_time = omp_get_wtime();
+            result_parallel = matrix1 * matrix2;
+            end_time = omp_get_wtime();
+            std::cout << "With parallelization (Eigen multi-thread): " << end_time - start_time << " seconds" << std::endl;
+        }
+        std::cout << "result_serial(0, 0): " << result_serial(0, 0) << ", result_parallel(0, 0): " << result_parallel(0, 0) << std::endl;
+        std::cout << "sum of result_serial: " << result_serial.sum() << ", sum of result_parallel: " << result_parallel.sum() << std::endl;
     }
 
     std::cout << "Vector inner product" << std::endl;
@@ -35,25 +42,29 @@ int main() {
         std::cout << "Vector size: " << size << std::endl;
         Eigen::VectorXd vector1 = Eigen::VectorXd::Random(size);
         Eigen::VectorXd vector2 = Eigen::VectorXd::Random(size);
-        double result1, result2;
+        double result_serial, result_parallel;
 
         double start_time, end_time;
 
         // Eigenの内部並列化を使用
-        Eigen::setNbThreads(1); // シングルスレッドで計算
-        std::cout << "Threads num: " << Eigen::nbThreads() << std::endl;
-        start_time = omp_get_wtime();
-        result1 = vector1.dot(vector2);
-        end_time = omp_get_wtime();
-        std::cout << "Without parallelization (Eigen single thread): " << end_time - start_time << " seconds" << std::endl;
+        {
+            Eigen::setNbThreads(1); // シングルスレッドで計算
+            std::cout << "Threads num: " << Eigen::nbThreads() << std::endl;
+            start_time = omp_get_wtime();
+            result_serial = vector1.dot(vector2);
+            end_time = omp_get_wtime();
+            std::cout << "Without parallelization (Eigen single thread): " << end_time - start_time << " seconds" << std::endl;
+        }
 
-        Eigen::setNbThreads(omp_get_max_threads()); // OpenMPの最大スレッド数を設定
-        std::cout << "Threads num: " << Eigen::nbThreads() << std::endl;
-        start_time = omp_get_wtime();
-        result2 = vector1.dot(vector2);
-        end_time = omp_get_wtime();
-        std::cout << "With parallelization (Eigen multi-thread): " << end_time - start_time << " seconds" << std::endl;
-        std::cout << "result1: " << result1 << ", result2: " << result2 << std::endl;
+        {
+            Eigen::setNbThreads(omp_get_max_threads()); // OpenMPの最大スレッド数を設定
+            std::cout << "Threads num: " << Eigen::nbThreads() << std::endl;
+            start_time = omp_get_wtime();
+            result_parallel = vector1.dot(vector2);
+            end_time = omp_get_wtime();
+            std::cout << "With parallelization (Eigen multi-thread): " << end_time - start_time << " seconds" << std::endl;
+        }
+        std::cout << "result_serial: " << result_serial << ", result_parallel: " << result_parallel << std::endl;
     }
 
     return 0;
